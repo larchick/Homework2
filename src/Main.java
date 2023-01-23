24464ffacbf7c94769a386e26d41b1904c724416
larchick <liliyagreis@gmail.com>
2023-01-23 16:39:13 +0300

    Мой первый коммит

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+}
+    public static void task1 () {
+    System.out.println ("Задача 1");
+            //код для задачи
+    }
+
+    public static void task2 () {
+        System.out.println ("Задача 2");
+        //код для задачи
+    }
+
+    public static void task3 () {
+        System.out.println ("Задача 3");
+        //код для задачи
+    }
+
+    public static void task4 () {
+        System.out.println ("Задача 4");
+        //код для задачи
+    }
+
+    public static void task5 () {
+        System.out.println ("Задача 5");
+        //код для задачи
+    }
+
+    public static void task6 () {
+        System.out.println ("Задача 6");
+        //код для задачи
+    }
+
+    public static void task7 () {
+        System.out.println ("Задача 7");
+        //код для задачи
+    }
+
+    public static void task8 () {
+        System.out.println ("Задача 8");
+        //код для задачи
+    }
+}
