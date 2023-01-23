5c8594f7a94a0bd2f0d5a7699da462a4b21df1a5
larchick <liliyagreis@gmail.com>
2023-01-23 17:52:32 +0300

    Ршение задачи 3

--- src/Main.java
+++ src/Main.java
@@ -34,7 +34,15 @@ public class Main {
 
     public static void task3 () {
         System.out.println ("Задача 3");
-        //код для задачи
+        var dog = 8.0;
+        dog = dog - 3.5;
+        System.out.println(dog);
+        var cat = 3.6;
+        cat = cat - 1.6;
+        System.out.println(cat);
+        var paper = 763789;
+        paper = paper - 7639;
+        System.out.println(paper);
     }
 
     public static void task4 () {
